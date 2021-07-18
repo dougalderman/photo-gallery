901c28e57d1d1d7ab6310ba3eb8665f3b749d26e
Doug Alderman <agnadcp-d1@yahoo.com>
2021-07-18 18:28:33 -0400

    Fixed bug by adding writeFile line to save file. Added mobile support from tutorial.

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Platform } from '@ionic/angular';
 
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Storage } from '@capacitor/storage';
+import { Capacitor } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +13,11 @@ import { Storage } from '@capacitor/storage';
 export class PhotoService {
   public photos: myPhoto[] = [];
   private PHOTO_STORAGE: string = "photos";
+  private platform: Platform;
 
-  constructor() { }
+  constructor(platform: Platform) {
+    this.platform = platform;
+   }
 
   public async callCamera(): Promise<Photo> {
     // Take a photo
@@ -51,21 +56,43 @@ export class PhotoService {
       directory: Directory.Data
     }
     console.log('fileObj: ', fileObj);
-    
-    // Use webPath to display the new image instead of base64 since it's 
-    // already loaded into memory
-    return {
-      filepath: fileName,
-      webviewPath: cameraPhoto.webPath
-    };
+    const savedFile = await Filesystem.writeFile(fileObj);
+
+    if (this.platform.is('hybrid')) {
+      // Display the new image by rewriting the 'file://' path to HTTP
+      // Details: https://ionicframework.com/docs/building/webview#file-protocol
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri)
+      }
+    }
+    else {
+      // Use webPath to display the new image instead of base64 since it's 
+      // already loaded into memory
+      return {
+        filepath: fileName,
+        webviewPath: cameraPhoto.webPath
+      };
+    }  
   }
 
   private async readAsBase64(cameraPhoto: Photo) {
-    // Fetch the photo, read as a blob, then convert to base64 foremat
-    const response = await fetch(cameraPhoto.webPath!);
-    const blob = await response.blob();
+    // "hybrid" will detect Cordova or Capacitor
+    if (this. platform.is('hybrid')) {
+      // Read the file into base64 format
+      const file = await Filesystem.readFile({
+        path: cameraPhoto.path
+      });
 
-    return await this.convertBlobToBase64(blob) as string;
+      return file.data;
+    }
+    else {
+      // Fetch the photo, read as a blob, then convert to base64 foremat
+      const response = await fetch(cameraPhoto.webPath!);
+      const blob = await response.blob();
+
+      return await this.convertBlobToBase64(blob) as string;
+    }
   }
 
   convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
@@ -83,18 +110,22 @@ export class PhotoService {
     console.log('photoList: ', photoList);
     this.photos = JSON.parse(photoList.value) || [];
 
-    // Display the photo by reading into base64 format
-    for (let photo of this.photos) {
-      // Read each saved photo's data from the Filesystem
-      console.log('photo to read:', photo);
-      const readFile = await Filesystem.readFile({
-        path: photo.filepath,
-        directory: Directory.Data
-      });
+    // Easiest way to detecdt when running on the web:
+    // "when the platform is NOT hybrid, do this"
+    if (!this.platform.is('hybrid')) {
+      // Display the photo by reading into base64 format
+      for (let photo of this.photos) {
+        // Read each saved photo's data from the Filesystem
+        console.log('photo to read:', photo);
+        const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: Directory.Data
+        });
 
-      // Web platform only: Load the photo as base64 data
-      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
-    }
+        // Web platform only: Load the photo as base64 data
+        photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+      }
+    }  
   }
 }
 
